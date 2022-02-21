c98aa0896604c346fbe40e1e2ea88ad6e3822256
Facemadel24 <54524611+Facemadel24@users.noreply.github.com>
2022-02-21 19:39:10 -0300

    actualisacion

---
+++ app.js
@@ -0,0 +1,7 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "Series.html");
+ifrm.setAttribute("scrolling", "");
+ifrm.style.width = "100%";
+ifrm.style.height = "180px";
+document.body.appendChild(ifrm);
+  
