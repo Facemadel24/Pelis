fd7d7f93e3c06f83388264e2bfd69cbdc155ca36
Facemadel24 <54524611+Facemadel24@users.noreply.github.com>
2022-02-21 19:44:46 -0300

    actualisacion

---
+++ JS/JS/DOM Entradas.js
@@ -0,0 +1,7 @@
+                     //INDEX
+//BANNER
+document.getElementById("Banners").src ="../img-ico/New Banners.png";
+
+                     //ENTRADAS
+                     //SERIES
+                     //ENTRADAS SERIES
