c9bdcdcb9330075f03c33b461835fa709618090b
Facemadel24 <54524611+Facemadel24@users.noreply.github.com>
2022-08-18 00:07:22 -0300

    Delete JS directory
    3

--- JS/DOM Entradas.js
+++
@@ -1,7 +0,0 @@
-                     //INDEX
-//BANNER
-document.getElementById("Banners").src ="../img-ico/New Banners.png";
-
-                     //ENTRADAS
-                     //SERIES
-                     //ENTRADAS SERIES
--- JS/DOM.js
+++
@@ -1,6 +0,0 @@
-                     //INDEX
-//BANNER
-document.getElementById("banners").src ="img-ico/New Banners.png";
-                     //ENTRADAS
-                     //SERIES
-                     //ENTRADAS SERIES
