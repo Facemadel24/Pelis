d22b8551d632b750588038a44e3a267c1d8d5a19
Facemadel24 <54524611+Facemadel24@users.noreply.github.com>
2022-08-18 00:09:05 -0300

    Delete app.js
    3

--- app.js
+++
@@ -1,7 +0,0 @@
-var ifrm = document.createElement("iframe");
-ifrm.setAttribute("src", "Series.html");
-ifrm.setAttribute("scrolling", "");
-ifrm.style.width = "100%";
-ifrm.style.height = "180px";
-document.body.appendChild(ifrm);
-  
